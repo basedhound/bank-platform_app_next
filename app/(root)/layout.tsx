5be21cdf087081325267e963e45e3826ce0276c2
Jozef S <franck.vukelic@gmail.com>
2024-06-11 19:14:08 +0700

    Add: root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,34 @@
-export default function RootLayout({
+import Image from "next/image";
+import { redirect } from "next/navigation";
+//
+import MobileNav from "@/components/shared/MobileNav";
+import Sidebar from "@/components/shared/Sidebar";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>) {  
+  // const loggedIn = await getLoggedInUser();
+  const loggedIn = {firstName:'John', lasname:'Doe'} 
+
+
+  if(!loggedIn) redirect('/sign-in')
+
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
