22e611dca99cf80737c7ffb45eae6fafb2097e01
Jozef S <franck.vukelic@gmail.com>
2024-06-14 20:31:03 +0700

    Add comments

--- lib/actions/bank.actions.ts
+++ lib/actions/bank.actions.ts
@@ -1,5 +1,5 @@
 "use server";
-
+//
 import {
   ACHClass,
   CountryCode,
@@ -8,10 +8,9 @@ import {
   TransferNetwork,
   TransferType,
 } from "plaid";
-
+//
 import { plaidClient } from "../plaid";
 import { parseStringify } from "../utils";
-
 import { getTransactionsByBankId } from "./transaction.actions";
 import { getBanks, getBank } from "./user.actions";
 
