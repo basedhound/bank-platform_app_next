1741b7fe22fc794f22a1315e39d3c0cf66d63faf
Jozef S <franck.vukelic@gmail.com>
2024-06-14 20:31:26 +0700

    Update logging user method

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,57 +1,64 @@
-import HeaderBox from '@/components/shared/HeaderBox'
-import RecentTransactions from '@/components/shared/RecentTransactions';
-import RightSidebar from '@/components/shared/RightSidebar';
-import TotalBalanceBox from '@/components/shared/TotalBalanceBox';
-// import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
-import { getLoggedInUser } from '@/lib/actions/user.actions';
+import HeaderBox from "@/components/shared/HeaderBox";
+import RecentTransactions from "@/components/shared/RecentTransactions";
+import RightSidebar from "@/components/shared/RightSidebar";
+import TotalBalanceBox from "@/components/shared/TotalBalanceBox";
+import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 
 const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
   const currentPage = Number(page as string) || 1;
   const loggedIn = await getLoggedInUser();
-  // const accounts = await getAccounts({ 
-    // userId: loggedIn.$id 
-  // })
 
-  // if(!accounts) return;
-  
-  // const accountsData = accounts?.data;
-  // const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+  // Fetch user's bank accounts
+  const accounts = await getAccounts({
+    userId: loggedIn.$id,
+  });
+  if (!accounts) return;
 
-  // const account = await getAccount({ appwriteItemId })
+  // Rename data
+  const accountsData = accounts?.data;
+
+  // Fetch the ID of that account in the database
+  const appwriteItemId = (id as string) || accounts?.data[0]?.appwriteItemId;
+
+  // Finally
+  const account = await getAccount({ appwriteItemId });
 
   return (
     <section className="home">
       <div className="home-content">
         <header className="home-header">
-          <HeaderBox 
+          <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.name || 'Guest'}
+            user={loggedIn?.firstName || "Guest"}
             subtext="Access and manage your account and transactions efficiently."
           />
 
-          <TotalBalanceBox 
-/*             accounts={accountsData}
+          <TotalBalanceBox
+            accounts={accountsData}
             totalBanks={accounts?.totalBanks}
-            totalCurrentBalance={accounts?.totalCurrentBalance} */
+            totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
 
-{        <RecentTransactions 
-      /*     accounts={accountsData}
+        {
+          <RecentTransactions
+          /*     accounts={accountsData}
           transactions={account?.transactions}
           appwriteItemId={appwriteItemId}
           page={currentPage} */
-        />}
+          />
+        }
       </div>
 
-     <RightSidebar 
-         user={loggedIn}
-        transactions={[]}
-        banks={[{currentBalance: 123.50}, {currentBalance: 500.50}]} 
+      <RightSidebar
+        user={loggedIn}
+        transactions={accounts?.transactions}
+        banks={accountsData?.slice(0,2)}
       />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
