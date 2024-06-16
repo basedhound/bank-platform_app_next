09c139f39868dc65477226bf5dbaa71c66450d39
Jozef S <franck.vukelic@gmail.com>
2024-06-16 18:36:53 +0700

    Add comments

--- lib/actions/transaction.actions.ts
+++ lib/actions/transaction.actions.ts
@@ -1,14 +1,16 @@
 "use server";
-
+//
 import { ID, Query } from "node-appwrite";
 import { createAdminClient } from "../appwrite";
 import { parseStringify } from "../utils";
 
+// Make ENV shorter by destructuring it
 const {
   APPWRITE_DATABASE_ID: DATABASE_ID,
   APPWRITE_TRANSACTION_COLLECTION_ID: TRANSACTION_COLLECTION_ID,
 } = process.env;
 
+/// Create Transaction
 export const createTransaction = async (transaction: CreateTransactionProps) => {
   try {
     const { database } = await createAdminClient();
@@ -30,6 +32,7 @@ export const createTransaction = async (transaction: CreateTransactionProps) => 
   }
 }
 
+/// Get Transactions by Bank ID
 export const getTransactionsByBankId = async ({bankId}: getTransactionsByBankIdProps) => {
   try {
     const { database } = await createAdminClient();
