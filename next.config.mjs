e91a1a95047f481f4aa82d3439683a3391066a99
Jozef S <franck.vukelic@gmail.com>
2024-07-09 14:28:14 +0700

    Update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,7 @@ export default withSentryConfig(nextConfig, {
 // Suppresses source map uploading logs during build
 silent: true,
 org: "frank-pn",
-project: "javascript-nextjs",
+project: "jsm_bank-horizon",
 }, {
 // For all available options, see:
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
